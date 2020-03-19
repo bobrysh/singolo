85e40213178a87125585e07f702571784d5cfc1a
Pavel Bobryshev <bobr.8590@gmail.com>
2020-03-19 13:13:03 +0300

    feat: change slider section

--- script.js
+++ script.js
@@ -14,3 +14,56 @@ function changeSection(elementsClass, activeClass, event) {
     event.target.closest(`.${elementsClass}`).classList.add(`${activeClass}`);
   }
 }
+
+// Slider
+const arrowLeftElement = document.querySelector(".arrow-left");
+const arrowRightElement = document.querySelector(".arrow-right");
+const slideElements = document.querySelectorAll(".slide");
+
+let currentSlide = 0;
+let isEnable = true;
+
+arrowLeftElement.addEventListener("click", () => {
+  if (isEnable) {
+    showPreviousSlide(currentSlide);
+  }
+});
+
+arrowRightElement.addEventListener("click", () => {
+  if (isEnable) {
+    showNextSlide(currentSlide);
+  }
+});
+
+function showPreviousSlide(currentSlide) {
+  hideSlide("to-right");
+  changeCurrentSlide(currentSlide - 1);
+  showSlide("from-left");
+}
+
+function showNextSlide(currentSlide) {
+  hideSlide("to-left");
+  changeCurrentSlide(currentSlide + 1);
+  showSlide("from-right");
+}
+
+function hideSlide(direction) {
+  isEnable = false;
+  slideElements[currentSlide].classList.add(direction);
+  slideElements[currentSlide].addEventListener("animationend", function() {
+    this.classList.remove("slide_active", direction);
+  });
+}
+
+function changeCurrentSlide(newSlide) {
+  currentSlide = (slideElements.length + newSlide) % slideElements.length;
+}
+
+function showSlide(direction) {
+  slideElements[currentSlide].classList.add("next-slide", direction);
+  slideElements[currentSlide].addEventListener("animationend", function() {
+    this.classList.remove("next-slide", direction);
+    this.classList.add("slide_active");
+    isEnable = true;
+  });
+}
