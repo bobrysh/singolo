752ba91a047e3a2206f382e8f78d6cc0e9eedf6e
Pavel Bobryshev <bobr.8590@gmail.com>
2020-03-29 16:50:23 +0300

    feat: add responsive & prettier remake

--- script.js
+++ script.js
@@ -1,188 +1,166 @@
-// Navigation
-const navigationElement = document.querySelector(".navigation");
+const NAVIGATION = document.getElementById('navigation');
+const PORTFOLIO_FILTER = document.getElementById('portfolio_buttons');
+const BUTTON_SUBMIT = document.getElementById('submit-button');
+const BUTTON_OK = document.getElementById('popup-close-btn');
+const PORTFOLIO_PICT = document.getElementById('portfolio-pictures');
+const PHONE_HORIZONTAL_SCREEN = document.getElementById('iphone-horizontal');
+const PHONE_VERTICAL_SCREEN = document.getElementById('iphone-vertical');
+const SHORT_MENU = document.getElementById('menu-icon');
 
-navigationElement.addEventListener("click", event => {
-  changeSection("navigation__item", "navigation__item_active", event);
-});
+// SLIDER
+document.querySelector('.slider__arrow_left').addEventListener('click', () => slider());
+document.querySelector('.slider__arrow_right').addEventListener('click', () => slider(false));
 
-function changeSection(elementsClass, activeClass, event) {
-  if (event.target.classList.contains(`${elementsClass}`)) {
-    const elements = document.querySelectorAll(`.${elementsClass}`);
-    elements.forEach(element => {
-      element.classList.remove(`${activeClass}`);
-    });
-    event.target.closest(`.${elementsClass}`).classList.add(`${activeClass}`);
-  }
+let firstSliderItem = true,
+sliderAnimationEnd = true;
+
+function slider(leftArrow = true) {
+    if (sliderAnimationEnd) {
+        sliderAnimationEnd = !sliderAnimationEnd;
+
+        const currentPicture = firstSliderItem ? 'slide-1' : 'slide-2';
+        const nextPicture = firstSliderItem ? 'slide-2' : 'slide-1';
+        const nextSlide = document.getElementsByClassName(nextPicture)[0];
+        const currentSlide = document.getElementsByClassName(currentPicture)[0];
+        const background = document.getElementsByClassName('slider__wrapper')[0];
+        let firstOffset, secondOffset;
+
+        if (leftArrow) {
+            firstOffset = 'slider_left-offset';
+            secondOffset = 'slider_right-offset';
+        } else {
+            firstOffset = 'slider_right-offset';
+            secondOffset = 'slider_left-offset';
+        }
+
+        nextSlide.classList.add(secondOffset);
+        nextSlide.addEventListener("transitionend",
+            () => {
+                nextSlide.classList.remove('hidden_opacity');
+                nextSlide.classList.remove(secondOffset);
+            }, {once: true});
+        currentSlide.classList.add(firstOffset);
+
+        background.classList.toggle('second-slide');
+        firstSliderItem = !firstSliderItem;
+
+        currentSlide.addEventListener("transitionend",
+            () => {
+                currentSlide.classList.add('hidden_opacity');
+                nextSlide.style.zIndex = '100';
+                currentSlide.style.zIndex = '1';
+                currentSlide.classList.remove(firstOffset);
+                sliderAnimationEnd = !sliderAnimationEnd;
+            }, {once: true});
+    }
 }
 
-// Slider
-const arrowLeft = document.querySelector(".arrow-left");
-const arrowRight = document.querySelector(".arrow-right");
-const slideElements = document.querySelectorAll(".slide");
-
-let currentSlide = 0;
-let isEnable = true;
-
-arrowLeft.addEventListener("click", () => {
-  if (isEnable) {
-    showPreviousSlide(currentSlide);
-  }
+//IPHONE BREAK
+PHONE_HORIZONTAL_SCREEN.addEventListener('click', (event) => {
+  if (event.target.id === 'iphone-horizontal-shadow') return;
+  document.getElementById('iphone-horizontal-screen').classList.toggle('screen_off');
 });
 
-arrowRight.addEventListener("click", () => {
-  if (isEnable) {
-    showNextSlide(currentSlide);
-  }
+PHONE_VERTICAL_SCREEN.addEventListener('click', (event) => {
+  if (event.target.id === 'iphone-vertical-shadow') return;
+  document.getElementById('iphone-vertical-screen').classList.toggle('screen_off');
 });
 
-function showPreviousSlide(currentSlide) {
-  hideSlide("to-right");
-  changeCurrentSlide(currentSlide - 1);
-  showSlide("from-left");
+  //PORTFOLIO SHUFFLE
+  PORTFOLIO_FILTER.addEventListener('click', (event) => {
+      if (event.target.tagName !== 'LI') return;
+      PORTFOLIO_FILTER.querySelectorAll('li')
+          .forEach(el => el.classList.remove('portfolio__button-item_active'));
+      event.target.classList.add('portfolio__button-item_active');
+      printPortfolio();
+  });
+
+  PORTFOLIO_PICT.addEventListener('click', (event) => {
+      if (event.target.tagName !== 'IMG') return;
+      PORTFOLIO_PICT.querySelectorAll('li')
+          .forEach(el => el.classList.remove('portfolio__image-item_active'));
+      event.target.parentNode.classList.add('portfolio__image-item_active');
+  });
+
+  function printPortfolio() {
+      let pictures = document.querySelectorAll('.portfolio__image-item');
+      let newArr = shuffle(Array.from(pictures));
+      document.getElementById('portfolio-pictures').innerHTML = newArr.reduce((acc, event) =>
+          typeof acc === 'string' ? acc + event.outerHTML : acc.outerHTML + event.outerHTML);
+  }
+
+  function shuffle(arr) {
+      let j, temp;
+      for (let i = arr.length - 1; i > 0; i--) {
+          j = Math.floor(Math.random() * (i + 1));
+          temp = arr[j];
+          arr[j] = arr[i];
+          arr[i] = temp;
+      }
+      return arr;
+  }
+
+// SHORT MENU
+SHORT_MENU.addEventListener('click', (event) => {
+  openShortMenu (event);
+});
+
+function openShortMenu (event) {
+  document.querySelector('.header-wrapper').classList.toggle('header__wrapper-close');
+  event.target.classList.toggle('menu-icon_active');
 }
 
-function showNextSlide(currentSlide) {
-  hideSlide("to-left");
-  changeCurrentSlide(currentSlide + 1);
-  showSlide("from-right");
+function closeShortMenu (event) {
+document.querySelector('.header-wrapper').classList.add('header__wrapper-close');
+document.querySelector('.material-icons').classList.remove('menu-icon_active');
 }
 
-function hideSlide(direction) {
-  isEnable = false;
-  slideElements[currentSlide].classList.add(direction);
-  slideElements[currentSlide].addEventListener("animationend", function() {
-    this.classList.remove("slide_active", direction);
+function slideToAnchor(event) {
+  const currentPos = window.scrollY;
+  const articles = document.querySelectorAll('main>article');
+
+  articles.forEach((element) => {
+      if (currentPos + 95 >= element.offsetTop && currentPos + 95 < (element.offsetTop + element.offsetHeight)) {
+          NAVIGATION.querySelectorAll('a').forEach((el) => {
+              el.classList.remove('navigation__item_active');
+              closeShortMenu(event);
+              if (element.getAttribute('class') === el.getAttribute('href').substring(1)) {
+                  el.classList.add('navigation__item_active');
+              }
+          })
+      }
   });
 }
+window.addEventListener('scroll', slideToAnchor);
 
-function changeCurrentSlide(newSlide) {
-  currentSlide = (slideElements.length + newSlide) % slideElements.length;
-}
+//SUBMIT FORM
+BUTTON_SUBMIT.addEventListener('click', (event) => {
 
-function showSlide(direction) {
-  slideElements[currentSlide].classList.add("next-slide", direction);
-  slideElements[currentSlide].addEventListener("animationend", function() {
-    this.classList.remove("next-slide", direction);
-    this.classList.add("slide_active");
-    isEnable = true;
-  });
-}
-
-
-// Screen switcher
-const slideFirstElement = document.querySelector(".slide-1");
-
-slideFirstElement.addEventListener("click", event => {
-  let phoneSelectedElement = event.target.closest(".base");
-
-  if (phoneSelectedElement) {
-    changeScreenMode(phoneSelectedElement);
+  if (document.querySelector('.form-quote').checkValidity()) {
+      event.preventDefault();
+      let messageText = getSubjectInput();
+      messageText += getDescriptionInput();
+      document.getElementById('result').innerText = messageText;
+      document.getElementById('quote-popup-block').classList.remove('hidden');
+      document.querySelector('.form-quote').reset();
   }
 });
 
-function changeScreenMode(phoneSelected) {
-  const screenSelectedElement = phoneSelected.querySelector(".screen");
+function getSubjectInput() {
+  const subject = document.getElementById('subject').value;
+  return subject ? `\nТема: ${subject}` : '\nБез темы';
+}
 
-  let currentMode;
-  let newMode;
-
-  if (screenSelectedElement.classList.contains("screen_on")) {
-    currentMode = "screen_on";
-    newMode = "screen_off";
-  } else {
-    currentMode = "screen_off";
-    newMode = "screen_on";
+function getDescriptionInput() {
+  const description = document.getElementById('description').value;
+  let sliced = description.slice(0, 300);
+  if (sliced.length < description.length) {
+      sliced += '...';
   }
-
-  screenSelectedElement.classList.remove(`${currentMode}`);
-  screenSelectedElement.classList.add(`${newMode}`);
+  return description ? `\nОписание: ${sliced}` : '\nБез описания';
 }
 
 
-//Portfolio
-const portfolioButtonsElements = document.querySelector(".portfolioSection__selector");
-const galleryElement = document.querySelector(".portfolioSection__images");
-const picturesElements = document.querySelectorAll(".portfolioSection__example");
-
-portfolioButtonsElements.addEventListener("click", event => {
-  if (event.target.classList.contains("selector__item")) {
-    reorderPictures();
-  }
-  setActive("selector__item", "button_bordered", event);
-  picturesElements.forEach(picture => {
-    picture.classList.remove("portfolioSection__example_active");
-  });
-});
-
-function reorderPictures() {
-  const firstPicture = galleryElement.children[0];
-  const firstPictureCopy = firstPicture.cloneNode();
-  firstPicture.remove();
-  galleryElement.append(firstPictureCopy);
-}
-
-galleryElement.addEventListener("click", event => {
-  setActive(`portfolioSection__example`, `portfolioSection__example_active`, event);
-});
-
-function setActive(elementsClass, activeClass, event) {
-  if (event.target.classList.contains(`${elementsClass}`)) {
-    const elements = document.querySelectorAll(`.${elementsClass}`);
-    elements.forEach(element => {
-      element.classList.remove(`${activeClass}`);
-    });
-    event.target.closest(`.${elementsClass}`).classList.add(`${activeClass}`);
-  }
-}
-
-
-//submit
-const formElement = document.querySelector("#form");
-const submitFormButton = document.querySelector("#contact-form-submit");
-const modalWrapperElement = document.querySelector(".modal-wrapper");
-const nameInput = document.querySelector("#name");
-const emailInput = document.querySelector("#email");
-const subjectInput = document.querySelector("#subject");
-const textareaInput = document.querySelector("#textarea");
-const modalSubjectElement = document.querySelector("#modal-subject");
-const modalDescriptionElement = document.querySelector("#modal-description");
-const modalCloseButton = document.querySelector("#modal-close-button");
-
-submitFormButton.addEventListener("click", event => {
-  event.preventDefault();
-
-  if (!nameInput.value) {
-    nameInput.classList.add("invalid");
-  }
-
-  if (!emailInput.value) {
-    emailInput.classList.add("invalid");
-  }
-
-  if (nameInput.value && emailInput.value) {
-    modalWrapperElement.classList.remove("visually-hidden");
-    modalSubjectElement.innerText = subjectInput.value
-      ? `Тема: ${subjectInput.value}`
-      : "Без темы";
-    modalDescriptionElement.innerText = textareaInput.value
-      ? `Описание: ${textareaInput.value}`
-      : "Без описания";
-    document.body.classList.add("locked");
-    document.body.style.paddingRight = `${scrollWidth}px`;
-  }
-});
-
-modalCloseButton.addEventListener("click", () => {
-  modalWrapperElement.classList.add("visually-hidden");
-  document.body.classList.remove("locked");
-  formElement.reset();
-  document.body.style.paddingRight = "";
-});
-
-nameInput.addEventListener("focus", function() {
-  this.classList.remove("invalid");
-});
-
-emailInput.addEventListener("focus", function() {
-  this.classList.remove("invalid");
+BUTTON_OK.addEventListener('click', () => {
+  document.getElementById('quote-popup-block').classList.add('hidden');
 });
